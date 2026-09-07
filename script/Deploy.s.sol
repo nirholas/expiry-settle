41f69ce4028c5945171e6c479012c5528e434310
nirholas <claudescammer@outlook.com>
2026-09-07 20:37:34 +0000

    chore: regenerate from the HookForge catalogue

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -36,6 +36,7 @@ import {ExpirySettleHook} from "src/hooks/ExpirySettleHook.sol";
 contract DeployExpirySettle is Script {
     uint160 internal constant FLAGS = uint160(Hooks.AFTER_INITIALIZE_FLAG | Hooks.BEFORE_ADD_LIQUIDITY_FLAG | Hooks.BEFORE_SWAP_FLAG);
 
+
     function run() external {
         IPoolManager manager = Chains.poolManager(block.chainid);
         require(address(manager) != address(0), "no Uniswap v4 PoolManager known for this chain");
